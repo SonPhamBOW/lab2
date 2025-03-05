c55459ccd8cb9c6c8e42cc08c2391f2c972d271c
chinhpxhhe171177 <chinhpxhe171177@fpt.edu.vn>
2025-03-05 10:06:18 +0700

    commit category

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,18 +1,19 @@
 // routes/categoryRoutes.js
 const express = require('express');
 const { getAllCategory, addCategory, updateCategoryById, getCategoryById, deleteCategoryById } = require('../controllers/category.controller');
+const AccessTokenValidator = require("../middlewares/customervalidator.middlewares");
 
 const router = express.Router();
 
 // GET: / Get all categories route 
 router.get('/', getAllCategory);
 // POST: /create Create a new Category 
-router.post('/create', addCategory);
+router.post('/create', AccessTokenValidator, addCategory);
 // PUT: /update Update a category
-router.put('/update/:categoryId', updateCategoryById);
+router.put('/update/:categoryId', AccessTokenValidator, updateCategoryById);
 // GET: /categoryId Get A Category By Id
 router.get('/:categoryId', getCategoryById);
 // DELETE: /categoryId Delete a category by id
-router.delete('/:categoryId', deleteCategoryById);
+router.delete('/:categoryId', AccessTokenValidator, deleteCategoryById);
 
 module.exports = router;
