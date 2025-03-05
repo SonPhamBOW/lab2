c359c8a7543604f56b8da0e41a969da296d8b265
duongtran1411 <duongtdhe172042@fpt.edu.vn>
2025-03-05 10:23:10 +0700

    fix get all orderorder

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -16,57 +16,61 @@ exports.createNewOrder = async (req, res, next) => {
       });
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 exports.getAllOrder = async (req, res, next) => {
-    try {
-        const customerId = req.users_decode.customer._id;
-        const orders = await Order.find({customer: customerId})
-        if(orders){
-            res.status(200).json({
-                "message":"get all order successful",
-                "order": orders    
-            })
-        }else{
-            res.status(400).json({
-                "message":"you need add product to order"
-            }) 
-        }
-                
-    } catch (error) {
-        next(error)
+  try {
+    const customerId = req.users_decode.customer._id;
+    const orders = await Order.find({ customer: customerId })
+      .populate("customer")
+      .populate({
+        path: "products.product",
+        model: "Product",
+      });
+    if (orders) {
+      res.status(200).json({
+        message: "get all order successful",
+        order: orders,
+      });
+    } else {
+      res.status(400).json({
+        message: "you need add product to order",
+      });
     }
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.updateStatusOrder = async (req, res, next) => {
-    try {
-        const id = req.params.id
-        const order = req.body;
-        if(!id){
-            res.status(400).json({
-                "message":"order is not existed"
-            })
-        }else{
-            const updateOrder = await Order.findOneAndUpdate(
-                {_id:id},
-                order,
-                {new: true, runValidators: true}
-            )
+  try {
+    const id = req.params.id;
+    const order = req.body;
+    if (!id) {
+      res.status(400).json({
+        message: "order is not existed",
+      });
+    } else {
+      const updateOrder = await Order.findOneAndUpdate({ _id: id }, order, {
+        new: true,
+        runValidators: true,
+      });
+      await Order.deleteMany;
 
-            if(!updateOrder){
-                res.status(400).json({
-                    "message":"Order can not found"
-                })
-            }else{
-                res.status(200).json({
-                    "message":"Order update successfully",
-                    "order":updateOrder
-                })
-            }
-        }
-    } catch (error) {
-        next(error)
+      if (!updateOrder) {
+        res.status(400).json({
+          message: "Order can not found",
+        });
+      } else {
+        res.status(200).json({
+          message: "Order update successfully",
+          order: updateOrder,
+        });
+      }
     }
+  } catch (error) {
+    next(error);
+  }
 };
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const route = express.Router();
 const {createNewOrder,getAllOrder,updateStatusOrder} = require('../controllers/order.controller');
 const router = require('./customer.routes');
+
+// POST: / - Create new order
 route.post('/create', createNewOrder)
+
+// GET: / - Get all order of customer
 route.get('/all',getAllOrder)
-route.put('/updatestatus',updateStatusOrder)
+
+// PUT: / - Update status and status payment 
+route.put('/updatestatus/:id',updateStatusOrder)
 
 module.exports = router;
